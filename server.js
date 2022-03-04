8cf81c5f9eb7a1bd811c46cfdffed36117808b7e
Gulizar Cicek <gulizar.cicek@corebit.tech>
2022-03-04 17:19:10 +0300

    Update PUT /todos/:id to use sequalize

--- server.js
+++ server.js
@@ -80,46 +80,37 @@ app.delete('/todos/:id', function(req,res){
 			res.status(204).send();
 		}
 	},function(){
-		res.status(500).send();
+		res.status(500).send(); 
 	});
 });
 
 
-
-// PUT / todos/:id
-app.put('/todos/:id', function(req,res){
-	var body = _.pick (req.body,'description','completed');
-	var validAttributes  = {};
-
-	if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
-		validAttributes.completed = body.completed;
-	}else if(body.hasOwnProperty('completed')){
-		return res.status(400).send();
-	}
-});
-
 app.put('/todos/:id',function(req,res){
 	var todoId = parseInt(req.params.id, 10);
-	var machecTodo = _.findWhere(todos,{id: todoId});
 	var body = _.pick(req.body,'description','completed');
-	var validAttributes = {};
+	var attributes = {};
 
-	if(!machecTodo){
-		return res.status(404).send();
-	}
-	if(body.hasOwnProperty('completed') &&_.isBoolean(body.completed)){
-		validAttributes.completed = body.completed;
-	}else if(body.hasOwnProperty('completed')){
-		return res.status(400).send();
+	if(body.hasOwnProperty('completed')){
+		attributes.completed = body.completed;
 	}
 
-	if(body.hasOwnProperty('description') &&_.isString(body.description) && body.description.trim().lenght>0){
-		validAttributes.description= body.description;	
-	}else if(body.hasOwnProperty('description')){
-		return res.status(400).send();
+	if(body.hasOwnProperty('description')){
+		attributes.description= body.description;	
 	}
-	_.extend(machecTodo,validAttributes);
-	res.json(machecTodo);
+
+	db.todo.findByPk(todoId).then(function(todo) {
+		if(todo){
+			todo.update(attributes).then(function(todo){
+				res.json(todo.toJSON());
+			}, function(e){
+				res.status(400).json(e);
+			});
+		}else {
+			res.status(404).send();
+		}
+	},function(){
+		res.status(500).send();
+	});
 });
 
 db.sequelize.sync().then(function(){
